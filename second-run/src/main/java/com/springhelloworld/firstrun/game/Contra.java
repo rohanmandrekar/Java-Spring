68bb65e16adeffd8a9e8d43481d21bed0990f68d
rohanmandrekar <rmaskaban@gmail.com>
2023-05-08 17:26:01 -0500

    dependency injection
    setter dependency, constructor dependency, autowired

--- second-run/src/main/java/com/springhelloworld/firstrun/game/Contra.java
+++ second-run/src/main/java/com/springhelloworld/firstrun/game/Contra.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("ContraQualifier")
+@Qualifier("ContraGaQualifier")
 public class Contra implements GameConsole{
 	
 	public void up() {
---
+++ second-run/src/main/java/com/springhelloworld/firstrun/game/GamingAppLauncherApplication.java
@@ -0,0 +1,26 @@
+package com.springhelloworld.firstrun.game;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+@ComponentScan("com.springhelloworld.firstrun.game")
+public class GamingAppLauncherApplication {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try(var context= new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class);){
+		
+		context.getBean(GameConsole.class).up();
+		
+		context.getBean(gameRunner.class).run();
+		
+		}
+	}
+
+}
