3e62b387888a14b644851baaab97a0f597332ef1
rohanmandrekar <rmaskaban@gmail.com>
2023-05-22 14:09:18 -0500

    used request parameters and model mapping (like jinja)

--- myfirstwebapp/myfirstwebapp/src/main/java/com/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/springboot/myfirstwebapp/login/LoginController.java
@@ -1,13 +1,17 @@
 package com.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	
 	@RequestMapping("login")
-	public String gotoLoginPage() {
+	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
+		//System.out.println("Request param is "+ name);
+		model.put("name", name);
 		return "login";
 		
 	}
--- myfirstwebapp/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ myfirstwebapp/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,6 @@
 <title>Login</title>
 </head>
 <body>
-<h1 style="color:blue;">LOGIN HERE!!</h1>
+<h1 style="color:blue;">LOGIN HERE ${name}!!</h1>
 </body>
 </html>
