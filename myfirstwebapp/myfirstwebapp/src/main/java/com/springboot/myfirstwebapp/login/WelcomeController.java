e8bf0850df5be497dcf9d5c5799b84a55fd32e4a
rohanmandrekar <rmaskaban@gmail.com>
2023-05-25 13:01:42 -0500

    removed login pages and added spring security dependency

---
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/springboot/myfirstwebapp/login/WelcomeController.java
@@ -0,0 +1,28 @@
+package com.springboot.myfirstwebapp.login;
+
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+
+
+@Controller
+@SessionAttributes("name")
+public class WelcomeController {
+	
+	
+
+	@RequestMapping(value="/",method=RequestMethod.GET )
+	public String gotoWelcomePage(ModelMap model) {
+		model.put("name", "Rohan");
+		return "welcome";
+		
+	}
+	
+	
+
+}
