6aae3281567bef5cdfca165b5d5ddc6e05bd6df6
rohanmandrekar <rmaskaban@gmail.com>
2023-05-27 15:44:47 -0500

    used path variable

--- restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 //exposing the rest API
@@ -17,6 +18,11 @@ public class HelloWorldController {
 	@GetMapping("hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("HELLOW WORLD");
+	}
+	
+	@GetMapping("hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean("HELLOW WORLD "+ name );
 	}
 
 }
