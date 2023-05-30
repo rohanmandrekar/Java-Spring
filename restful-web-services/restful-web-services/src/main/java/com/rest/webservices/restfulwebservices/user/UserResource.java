93df5cb2dac1efd532cc34cc27bad37429a86e1d
rohanmandrekar <rmaskaban@gmail.com>
2023-05-30 16:00:02 -0500

    added correct http response for create method and returned location of added value

--- restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,12 +1,15 @@
 package com.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -32,8 +35,13 @@ public class UserResource {
 	}
 	
 	@PostMapping("/users")
-	public void createlUser(@RequestBody User user){
-			service.save(user);
+	public ResponseEntity<User> createlUser(@RequestBody User user){
+			User savedUser=service.save(user);
+			URI location=ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}")
+					.buildAndExpand(savedUser.getId())
+					.toUri();
+			return ResponseEntity.created(location).build();
 	}
 	
 	
