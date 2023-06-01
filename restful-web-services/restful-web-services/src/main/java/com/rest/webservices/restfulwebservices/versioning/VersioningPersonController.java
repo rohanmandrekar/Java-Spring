9af41b902bfb99316050baeebc778f4e3c78745b
rohanmandrekar <rmaskaban@gmail.com>
2023-06-01 15:37:29 -0500

    multiple versioning methods implemented

--- restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -14,4 +14,34 @@ public class VersioningPersonController {
 		return new PersonV2(new Name("Rohan", "Mandrekar"));
 	
 	}
+	
+	@GetMapping(path="person", params="version=1")
+	public PersonV1 getFirstVersionofPersonRequestParam() {
+		return new PersonV1("Rohan Mandrekar");
+	}
+	
+	@GetMapping(path="person", params="version=2")
+	public PersonV2 getSecondtVersionofPersonRequestParam() {
+		return new PersonV2(new Name("Rohan", "Mandrekar"));
+	}
+	
+	@GetMapping(path="person/header", headers="X-API-VERSION=1")
+	public PersonV1 getFirstVersionofPersonRequestHeader() {
+		return new PersonV1("Rohan Mandrekar");
+	}
+	
+	@GetMapping(path="person/header", headers="X-API-VERSION=2")
+	public PersonV2 getSecondtVersionofPersonRequestHeader() {
+		return new PersonV2(new Name("Rohan", "Mandrekar"));
+	}
+	
+	@GetMapping(path="person/accept", produces="application/vnd.company.app-v1+json")
+	public PersonV1 getFirstVersionofPersonAcceptHeader() {
+		return new PersonV1("Rohan Mandrekar");
+	}
+	
+	@GetMapping(path="person/accept", produces="application/vnd.company.app-v2+json")
+	public PersonV2 getSecondtVersionofPersonAcceptHeader() {
+		return new PersonV2(new Name("Rohan", "Mandrekar"));
+	}
 }
