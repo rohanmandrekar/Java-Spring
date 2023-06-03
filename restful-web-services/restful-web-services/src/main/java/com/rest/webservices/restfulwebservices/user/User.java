a080a62c8dcec19e5ca422bd4894706fe6148ebe
rohanmandrekar <rmaskaban@gmail.com>
2023-06-03 17:36:06 -0500

    filtering values of json sent

--- restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -2,14 +2,18 @@ package com.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class User {
 	private int id;
 	@Size(min=2, message="Name should have atleast 2 charecters")
+	@JsonProperty("user_name")
 	private String name;
 	@Past(message="Birthdate should be in the past")
+	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 	
 	public User(int id, String name, LocalDate birthDate) {
