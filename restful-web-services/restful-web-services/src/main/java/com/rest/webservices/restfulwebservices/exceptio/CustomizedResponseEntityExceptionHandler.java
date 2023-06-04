3dcb8cce3d45f11eb22948dd1d879267f8c42ad9
rohanmandrekar <rmaskaban@gmail.com>
2023-06-04 15:33:59 -0500

    added data to h2 database

--- restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/exceptio/CustomizedResponseEntityExceptionHandler.java
+++ restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/exceptio/CustomizedResponseEntityExceptionHandler.java
@@ -1,12 +1,12 @@
 package com.rest.webservices.restfulwebservices.exceptio;
 
-import java.net.http.HttpHeaders;
+//import java.net.http.HttpHeaders;
 import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+//import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -38,11 +38,11 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 //		return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
 //	}
 	
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(
-			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-		ErrorDetails errorDetails= new ErrorDetails(LocalDateTime.now(), ex.getFieldError().getDefaultMessage(),request.getDescription(false));
-		return new ResponseEntity<Object>(errorDetails,HttpStatus.BAD_REQUEST);
-		
-	}
+//	@Override
+//	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+//			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+//		ErrorDetails errorDetails= new ErrorDetails(LocalDateTime.now(), ex.getFieldError().getDefaultMessage(),request.getDescription(false));
+//		return new ResponseEntity<Object>(errorDetails,HttpStatus.BAD_REQUEST);
+//		
+//	}
 }
---
+++ restful-web-services/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user_details(id, birth_date,name)
+values(1001,current_date(),'Rohan');
+insert into user_details(id, birth_date,name)
+values(1002,current_date(),'Sarina'); 
+insert into user_details(id, birth_date,name)
+values(1003,current_date(),'Tester');
