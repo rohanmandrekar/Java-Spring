a49583a2bf1588463a9a990f0a18840718609a69
rohanmandrekar <rmaskaban@gmail.com>
2023-06-04 17:34:18 -0500

    created post entity with many to one relation ship with user entity

--- restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -1,10 +1,14 @@
 package com.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -23,6 +27,10 @@ public class User {
 	@Past(message="Birthdate should be in the past")
 	//@JsonProperty("birth_date")
 	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy="user")
+	@JsonIgnore
+	private List<Post> posts;
 	
 	public User(int id, String name, LocalDate birthDate) {
 		super();
--- restful-web-services/restful-web-services/src/main/resources/data.sql
+++ restful-web-services/restful-web-services/src/main/resources/data.sql
@@ -4,3 +4,15 @@ insert into user_details(id, birth_date,name)
 values(1002,current_date(),'Sarina'); 
 insert into user_details(id, birth_date,name)
 values(1003,current_date(),'Tester');
+
+insert into post(id,description,user_id)
+values(1,'Heyyy snapchat! Welcome to my vlog!',1002);
+
+insert into post(id,description,user_id)
+values(2,'hello world',1001);
+
+insert into post(id,description,user_id)
+values(4,'I wont drink water',1002);
+
+insert into post(id,description,user_id)
+values(4,'Drink water',1001);
