52f38a6eefd525c3270d4e6db517bf536b86acb6
rohanmandrekar <rmaskaban@gmail.com>
2023-06-09 13:23:22 -0500

    learnt how to make react components

--- React/frontend/todo-app/src/App.js
+++ React/frontend/todo-app/src/App.js
@@ -1,12 +1,46 @@
 
+import { Component } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       My Todo Application
+      <FirstComponent></FirstComponent>
+      <SeconfComponent></SeconfComponent>
+      <ThirdComponent/>
+      <FourthComponent/>
     </div>
   );
+}
+
+
+function FirstComponent(){
+  return(
+    <div className='FirstComponent'>First Component</div>
+  );
+}
+
+function SeconfComponent(){
+  return(
+    <div className='SecondComponent'>Second Component</div>
+  );
+}
+
+class ThirdComponent extends Component{
+  render(){
+    return(
+      <div className='ThirdComponent'>Third Component</div>
+    );
+  }
+}
+
+class FourthComponent extends Component{
+  render(){
+    return(
+      <div className='FourthComponent'>Fourth Component</div>
+    );
+  }
 }
 
 export default App;
