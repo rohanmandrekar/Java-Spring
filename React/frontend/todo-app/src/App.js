767a03bddb41e4de5d3bde3417b83bd6018442be
rohanmandrekar <rmaskaban@gmail.com>
2023-06-09 23:31:36 -0500

    moved each component to their own module

--- React/frontend/todo-app/src/App.js
+++ React/frontend/todo-app/src/App.js
@@ -1,46 +1,23 @@
 
-import { Component } from 'react';
+// import { Component } from 'react';
 import './App.css';
+import FirstComponent from './components/learning-examples/FirstComponent';
+import SecondComponent from './components/learning-examples/SecondComponent';
+import ThirdComponent from './components/learning-examples/ThirdComponent';
+import FourthComponent from './components/learning-examples/FourthComponent';
+import {FifthComponent} from './components/learning-examples/FirstComponent';
 
 function App() {
   return (
     <div className="App">
       My Todo Application
       <FirstComponent></FirstComponent>
-      <SeconfComponent></SeconfComponent>
+      <SecondComponent></SecondComponent>
       <ThirdComponent/>
       <FourthComponent/>
+      <FifthComponent/>
     </div>
   );
-}
-
-
-function FirstComponent(){
-  return(
-    <div className='FirstComponent'>First Component</div>
-  );
-}
-
-function SeconfComponent(){
-  return(
-    <div className='SecondComponent'>Second Component</div>
-  );
-}
-
-class ThirdComponent extends Component{
-  render(){
-    return(
-      <div className='ThirdComponent'>Third Component</div>
-    );
-  }
-}
-
-class FourthComponent extends Component{
-  render(){
-    return(
-      <div className='FourthComponent'>Fourth Component</div>
-    );
-  }
-}
+} 
 
 export default App;
