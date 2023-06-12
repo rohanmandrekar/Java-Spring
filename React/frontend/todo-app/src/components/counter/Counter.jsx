e99d8bc99bb552c46c625a8ae440e312af555ae9
rohanmandrekar <rmaskaban@gmail.com>
2023-06-12 16:04:04 -0500

    added state to counter app

--- React/frontend/todo-app/src/components/counter/Counter.jsx
+++ React/frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,14 +1,17 @@
 import './Counter.css'
+import {useState} from 'react'
 
 export default function Counter(){
+
+    const [count,setCount] = useState(0);
   
     function incrementCounterFunction(){
-        console.log('incremented')
+        setCount(count+1)
     }
     return(
         <div className="Counter">
             
-            <span className="count">0</span><br></br>
+            <span className="count">{count}</span><br></br>
             <button className="counterButton" onClick={incrementCounterFunction}>+1</button>
             </div> 
     );
