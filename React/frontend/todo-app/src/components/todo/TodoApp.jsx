521ce094536349743b764ea71d9caad62d918ebe
rohanmandrekar <rmaskaban@gmail.com>
2023-06-13 16:46:33 -0500

    added state to login page and made default field value editable using state

--- React/frontend/todo-app/src/components/todo/TodoApp.jsx
+++ React/frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -1,3 +1,7 @@
+import { useState } from 'react'
+import './TodoApp.css'
+
+
 export default function TodoApp(){
     return(
         <div className="TodoApp">
@@ -10,17 +14,31 @@ export default function TodoApp(){
 }
 
 function LoginComponent(){
+
+    const [username, setUser] = useState("user")
+    const [password,setPassword]=useState("pass")
+
+    function handleUsernameChange(event){
+        // console.log(event.target.value);
+        setUser(event.target.value)
+    }
+
+    function handlePasswordChange(event){
+        // console.log(event.target.value);
+        setPassword(event.target.value)
+    }
+
     return(
         <div className="Login">
             <div className="LoginForm">
                 <div>
                     <label>Username</label>
-                    <input type="text" name= "username"/>
+                    <input type="text" name= "username" value={username} onChange={handleUsernameChange}/>
                 </div>
 
                 <div>
                     <label>Password</label>
-                    <input type="password" name= "password"/>
+                    <input type="password" name= "password" value={password} onChange={handlePasswordChange}/>
                 </div>
 
                 <div>
