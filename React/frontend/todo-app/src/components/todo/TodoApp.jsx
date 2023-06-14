9cd1ff34228ce919907aa0935755ef7ca6edbc06
rohanmandrekar <rmaskaban@gmail.com>
2023-06-13 21:14:41 -0500

    added hardcoded authentication

--- React/frontend/todo-app/src/components/todo/TodoApp.jsx
+++ React/frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -17,6 +17,8 @@ function LoginComponent(){
 
     const [username, setUser] = useState("user")
     const [password,setPassword]=useState("pass")
+    const [showSuccess,setShowSuccess]=useState(false)
+    const [showError,setShowError]=useState(false)
 
     function handleUsernameChange(event){
         // console.log(event.target.value);
@@ -28,8 +30,20 @@ function LoginComponent(){
         setPassword(event.target.value)
     }
 
+    function handleSubmit(){
+        if(username==='user' && password==='pass'){
+            setShowSuccess(true)
+        }
+        else{
+            setShowError(true)
+        }
+    }
+
     return(
         <div className="Login">
+            <SuccessMessageComponent showSuccess={showSuccess}/>
+            <ErrorMessageComponent showError={showError}/>
+            
             <div className="LoginForm">
                 <div>
                     <label>Username</label>
@@ -42,12 +56,34 @@ function LoginComponent(){
                 </div>
 
                 <div>
-                    <button type="button" name="login">Login</button>
+                    <button type="button" name="login" onClick={handleSubmit} >Login</button>
                 </div>
 
             </div>
         </div>
     )
+}
+
+function SuccessMessageComponent({showSuccess}){
+    if(showSuccess){
+    return(
+        <div className='authenticatedMessage' style={{color:"green"}}>Authenticated Succesfully</div>
+    )
+    }
+    else{
+        return null
+    }
+}
+
+function ErrorMessageComponent({showError}){
+    if(showError){
+    return(
+        <div className='errorMessage' style={{color:"red"}}>Authention Failed</div>
+    )
+    }
+    else{
+        return null
+    }
 }
 
 function WelcomeComponent(){
