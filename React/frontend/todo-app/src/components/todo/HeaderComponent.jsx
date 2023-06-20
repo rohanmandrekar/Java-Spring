dc478f6fc668e53a83d0633363c116dee8162c36
rohanmandrekar <rmaskaban@gmail.com>
2023-06-19 22:35:05 -0500

    used use auth

--- React/frontend/todo-app/src/components/todo/HeaderComponent.jsx
+++ React/frontend/todo-app/src/components/todo/HeaderComponent.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom"
-import { AuthContext } from './security/AuthContext'
-import { useContext } from "react"
+import { useAuth } from './security/AuthContext'
+
 
 export default function HeaderComponent(){
-    const authContext=useContext(AuthContext)
-    console.log(authContext.number)
+    const authContext=useAuth()
+
+    // console.log(authContext.number)
+    
     return(
         <header className="border-bottom border-light border-5 mb-5 p-2">
         <div className="container">
--- React/frontend/todo-app/src/components/todo/security/AuthContext.js
+++ React/frontend/todo-app/src/components/todo/security/AuthContext.js
@@ -1,9 +1,15 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const AuthContext=createContext()
 
+export const useAuth=()=> useContext(AuthContext)
+
 export default function AuthProvider({children}){
     const [number,setNumber] = useState(10)
+
+    setInterval(
+        () => setNumber(number+1), 100
+    )
     return(
         <AuthContext.Provider value={{number}}>
             {children}
