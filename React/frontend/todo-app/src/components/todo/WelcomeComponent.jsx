af4ff12fce2feae25b2e704bcc30ed212ff5be2c
rohanmandrekar <rmaskaban@gmail.com>
2023-06-22 14:24:42 -0500

    added an axiom call in react to access spring API

--- React/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ React/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,12 +1,33 @@
 import { useParams,Link } from "react-router-dom"
+import axios from 'axios'
+
 export default function WelcomeComponent(){
 
     const {username}= useParams()
 
+    function callHelloWorld(){
+        axios.get('http://localhost:8080/hello-world',{}, {auth: {
+            username: 'user',
+            password: 'pass'
+          }})
+            .then((response)=>successfulResponse(response))
+            .catch((error)=>errorResponse(error))
+            .finally( ()=>console.log('cleanup'))
+    }
+    function successfulResponse(response){
+        console.log(response)
+    }
+    function errorResponse(error){
+        console.log(error)
+    }
+
     return(
         <div className="Welcome">
             <h1>Welcome {username}</h1>
-          <h2><Link to="/todos">Go to TODOs</Link></h2> 
+            <h2><Link to="/todos">Go to TODOs</Link></h2> 
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorld}>Hello World</button>
+            </div>
         </div>
     )
 }
