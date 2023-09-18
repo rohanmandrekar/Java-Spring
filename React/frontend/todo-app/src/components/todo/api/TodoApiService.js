bc89d71f3fbb7a1002dc5e6b23cf32710e6f2df9
rohanmandrekar <rmaskaban@gmail.com>
2023-09-18 14:30:21 -0500

    addes find todo by id and delete todo by id API

--- React/frontend/todo-app/src/components/todo/api/TodoApiService.js
+++ React/frontend/todo-app/src/components/todo/api/TodoApiService.js
@@ -12,4 +12,6 @@ const apiClient= axios.create(
 
 export const retrieveAllTodosForUsername = (username) => apiClient.get(`http://localhost:8080/users/${username}/todos`)
 
+export const retriveTodoById = (username, id)=> apiClient.get(`http://localhost:8080/users/${username}/todos.${id}`)
+
 // export const RetrievePathVariableBean = (username) => apiClient.get(`http://localhost:8080/hello-world/path-variable/${username}`)
