6f3c6d439e96e59be65e2f2d3efa74caee64f04a
rohanmandrekar <rmaskaban@gmail.com>
2023-10-12 18:36:53 -0500

    used stubs

---
+++ mockito-demo/mockito-demo/src/test/java/com/mockito/mockitodemo/business/SomeBusinessImplTest.java
@@ -0,0 +1,41 @@
+package com.mockito.mockitodemo.business;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SomeBusinessImplTest {
+
+	@Test
+	void testFindGreatestFromAllData() {
+		DataService dataServiceStub= new DataServiceStub1();
+		SomeBusinessImpl businessImpl= new SomeBusinessImpl(dataServiceStub);
+		assertEquals(25, businessImpl.findTheGreatestFromAllData());
+	}
+	
+	@Test
+	void testFindGreatestFroOneValue() {
+		DataService dataServiceStub= new DataServiceStub2();
+		SomeBusinessImpl businessImpl= new SomeBusinessImpl(dataServiceStub);
+		assertEquals(35, businessImpl.findTheGreatestFromAllData());
+	
+}
+
+
+class DataServiceStub1 implements DataService{
+	@Override
+	public int[] retrieveAllData() {
+		
+		return new int[] {25,15,5};
+	}
+	
+}
+
+class DataServiceStub2 implements DataService{
+	@Override
+	public int[] retrieveAllData() {
+		
+		return new int[] {35};
+	}
+}	
+}
