c4f4df70ab0465c10a76ea098d6e7b5448124fcd
rohanmandrekar <rmaskaban@gmail.com>
2023-10-12 12:32:56 -0500

    ran first junit test

---
+++ JUnit/test/com/junit/MyMathTest.java
@@ -0,0 +1,19 @@
+package com.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyMathTest {
+
+	@Test
+	void test() {
+		int[] numbers = {1,2,3,4};
+		MyMath math= new MyMath();
+		int result=math.calculateSum(numbers);
+//		System.out.println(result);
+		int expectedResult=10;
+		assertEquals(expectedResult,result);
+	}
+
+}
