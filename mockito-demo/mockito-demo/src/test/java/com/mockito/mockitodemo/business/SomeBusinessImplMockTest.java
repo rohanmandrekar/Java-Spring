839cf9b78997908d42b2d8feeb340f158e989005
rohanmandrekar <rmaskaban@gmail.com>
2023-10-12 19:11:15 -0500

    simplified mockito code using injection

--- mockito-demo/mockito-demo/src/test/java/com/mockito/mockitodemo/business/SomeBusinessImplMockTest.java
+++ mockito-demo/mockito-demo/src/test/java/com/mockito/mockitodemo/business/SomeBusinessImplMockTest.java
@@ -1,26 +1,38 @@
 package com.mockito.mockitodemo.business;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class SomeBusinessImplMockTest {
-
+	@Mock
+	private DataService dataServiceMock;
+	
+	@InjectMocks
+	private SomeBusinessImpl businessImpl;
+	
+	
 	@Test
 	void testFindGreatestFromAllData() {
-		DataService dataServiceMock = mock(DataService.class);
 		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {25,15,5});
-		SomeBusinessImpl businessImpl= new SomeBusinessImpl(dataServiceMock);
 		assertEquals(25, businessImpl.findTheGreatestFromAllData());
 	}
 	
 	@Test
 	void testFindGreatestFromOneValue() {
-		DataService dataServiceMock = mock(DataService.class);
 		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {35});
-		SomeBusinessImpl businessImpl= new SomeBusinessImpl(dataServiceMock);
 		assertEquals(35, businessImpl.findTheGreatestFromAllData());
+	}
+	
+	@Test
+	void testFindGreatestEmptyArray() {
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {});
+		assertEquals(Integer.MIN_VALUE, businessImpl.findTheGreatestFromAllData());
 	}
 }
