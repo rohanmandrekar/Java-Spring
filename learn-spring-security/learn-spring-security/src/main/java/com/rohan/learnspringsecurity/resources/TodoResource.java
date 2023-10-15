0201f053353d07b5d81633fc3c0d3a68a9049a6a
rohanmandrekar <rmaskaban@gmail.com>
2023-10-15 14:23:44 -0500

    created a few dummy apis to explore csrf

---
+++ learn-spring-security/learn-spring-security/src/main/java/com/rohan/learnspringsecurity/resources/TodoResource.java
@@ -0,0 +1,39 @@
+package com.rohan.learnspringsecurity.resources;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoResource {
+	private static final List<Todo> List_todos = List.of(new Todo("rohan","Learn AWS"),
+			new Todo("rohan","Get a job"));
+	
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@GetMapping("/todos")
+	public List<Todo> retrieveAllTodos() {
+		return List_todos;
+	}
+	
+	@GetMapping("/users/{username}/todos")
+	public Todo retrieveTodoforUser(@PathVariable String username) {
+		return List_todos.get(0);
+	}
+	
+	@PostMapping("/users/{username}/todos")
+	public void createTodoforUser(@PathVariable String username, @RequestBody Todo todo) {
+		logger.info("Create {} for {}",todo, username);
+	
+	}
+
+}
+
+record Todo (String username, String description) {}
