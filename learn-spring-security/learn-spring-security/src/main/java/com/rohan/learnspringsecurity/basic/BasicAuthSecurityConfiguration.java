4b7a29eac5d805669ebcd58cf5a4be504c903e4d
rohanmandrekar <rmaskaban@gmail.com>
2023-10-19 16:57:51 -0500

    added multiple in memory users

--- learn-spring-security/learn-spring-security/src/main/java/com/rohan/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/learn-spring-security/src/main/java/com/rohan/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +23,23 @@ public class BasicAuthSecurityConfiguration {
 		http.csrf().disable();
 		return http.build();
 	
+	}
+	
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		var user= User.withUsername("rohan")
+		.password("{noop}pass")
+		.roles("USER")
+		.build();
+		
+		var admin= User.withUsername("admin")
+				.password("{noop}pass")
+				.roles("ADMIN")
+				.build();
+		
+		
+		
+		return new InMemoryUserDetailsManager(user,admin);
 	}
 }
