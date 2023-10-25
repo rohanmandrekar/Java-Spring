57803704d62ae0ad9d6f48a553af97d33c0d0f21
rohanmandrekar <rmaskaban@gmail.com>
2023-10-24 21:39:15 -0500

    created an Oauth project

--- learn-spring-security/learn-spring-security/src/main/java/com/rohan/learnspringsecurity/resources/TodoResource.java
+++ learn-spring-security/learn-spring-security/src/main/java/com/rohan/learnspringsecurity/resources/TodoResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +31,7 @@ public class TodoResource {
 	@PreAuthorize("hasRole('USER') and #username == authentication.name")
 	@GetMapping("/users/{username}/todos")
 	@RolesAllowed({"ADMIN", "USER"})
+	@Secured({"ADMIN", "USER"})
 	@PostAuthorize("returnObject.username == 'rohan'")
 	public Todo retrieveTodoforUser(@PathVariable String username) {
 		return List_todos.get(0);
